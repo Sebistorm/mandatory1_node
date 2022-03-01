ae290f370dd929a17ac09dcc38cfaa67edd4f039
Sebistorm <seba7843@stud.kea.dk>
2022-03-01 19:37:15 +0100

    styling & layout

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const fs = require("fs");
 // Components
 const top = fs.readFileSync("./public/components/top.html").toString();
 const bottom = fs.readFileSync("./public/components/bottom.html").toString();
+const sidebar = fs.readFileSync("./public/components/sidebar.html").toString();
 
 // Pages
 const frontpage = fs.readFileSync("./public/frontpage/index.html");
 
 // Pages with components
-const frontpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Nodejs Dokumentation") + frontpage + bottom;
+const frontpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Nodejs Dokumentation") + sidebar + frontpage + bottom;
 
 app.get("/", (req, res) => {
     res.send(frontpagePage);
