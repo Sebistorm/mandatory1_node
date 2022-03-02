2a87e35dfe019996c8a405922d25698697530ffb
LAPTOP-4141SHPJ\seb99 <seb994@live.dk>
2022-03-02 21:29:47 +0100

    variables, loops, app & crud

--- app.js
+++ app.js
@@ -11,13 +11,38 @@ const bottom = fs.readFileSync("./public/components/bottom.html").toString();
 const sidebar = fs.readFileSync("./public/components/sidebar.html").toString();
 
 // Pages
-const frontpage = fs.readFileSync("./public/frontpage/index.html");
+const frontpage = fs.readFileSync("./public/pages/index.html");
+const variablespage = fs.readFileSync("./public/pages/variables.html");
+const loopspage = fs.readFileSync("./public/pages/loops.html")
+const apppage = fs.readFileSync("./public/pages/app.html")
+const crudpage = fs.readFileSync("./public/pages/crud.html")
 
 // Pages with components
 const frontpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Nodejs Dokumentation") + sidebar + frontpage + bottom;
+const variablespagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Variables") + sidebar + variablespage + bottom;
+const loopspagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Loops") + sidebar + loopspage + bottom;
+const apppagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - app.js") + sidebar + apppage + bottom;
+const crudpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Pure Data CRUDable REST API") + sidebar + crudpage + bottom;
 
+// get
 app.get("/", (req, res) => {
     res.send(frontpagePage);
+})
+
+app.get("/variables", (req, res) => {
+    res.send(variablespagePage);
+})
+
+app.get("/loops", (req, res) => {
+    res.send(loopspagePage);
+})
+
+app.get("/app", (req, res) => {
+    res.send(apppagePage);
+})
+
+app.get("/crud", (req, res) => {
+    res.send(crudpagePage);
 })
 
 
