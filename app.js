efe4e64fd9fecddf21918c3419cbc2fe9a834e40
LAPTOP-4141SHPJ\seb99 <seb994@live.dk>
2022-03-03 23:41:49 +0100

    nodemon, router, commands, footer css, sidebar sticky, index redirecting to variables

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ const sidebar = fs.readFileSync("./public/components/sidebar.html").toString();
 // Pages
 const frontpage = fs.readFileSync("./public/pages/index.html");
 const variablespage = fs.readFileSync("./public/pages/variables.html");
-const loopspage = fs.readFileSync("./public/pages/loops.html")
-const apppage = fs.readFileSync("./public/pages/app.html")
-const crudpage = fs.readFileSync("./public/pages/crud.html")
+const loopspage = fs.readFileSync("./public/pages/loops.html");
+const apppage = fs.readFileSync("./public/pages/app.html");
+const crudpage = fs.readFileSync("./public/pages/crud.html");
+const nodemonpage = fs.readFileSync("./public/pages/nodemon.html");
+const routerpage = fs.readFileSync("./public/pages/router.html");
+const commandspage =fs. readFileSync("./public/pages/commands.html");
 
 // Pages with components
 const frontpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Nodejs Dokumentation") + sidebar + frontpage + bottom;
@@ -23,10 +26,13 @@ const variablespagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Varia
 const loopspagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Loops") + sidebar + loopspage + bottom;
 const apppagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - app.js") + sidebar + apppage + bottom;
 const crudpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Pure Data CRUDable REST API") + sidebar + crudpage + bottom;
+const nodemonpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Nodemon") + sidebar + nodemonpage + bottom;
+const routerpagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Router") + sidebar + routerpage + bottom;
+const commandspagePage = top.replace("%%DOCUMENT_TITLE%%", "Mandatory 1 - Commands") + sidebar + commandspage + bottom;
 
 // get
 app.get("/", (req, res) => {
-    res.send(frontpagePage);
+    res.redirect("variables");
 })
 
 app.get("/variables", (req, res) => {
@@ -44,6 +50,19 @@ app.get("/app", (req, res) => {
 app.get("/crud", (req, res) => {
     res.send(crudpagePage);
 })
+
+app.get("/nodemon", (req, res) => {
+    res.send(nodemonpagePage);
+})
+
+app.get("/router", (req, res) => {
+    res.send(routerpagePage);
+})
+
+app.get("/commands", (req, res) => {
+    res.send(commandspagePage);
+})
+
 
 
 const PORT = process.env.PORT || 8080;
